09d81f86b533af81b7e9997409983903830a1c20
farraselfahd <farraselfahd2000@upi.edu>
2021-05-09 22:41:53 +0700

    new

---
+++ db_gamepbo.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 09, 2021 at 05:41 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_gamepbo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `highscore`
+--
+
+CREATE TABLE `highscore` (
+  `id` int(11) NOT NULL,
+  `Username` varchar(200) NOT NULL,
+  `Score` int(11) NOT NULL,
+  `Duration` int(11) NOT NULL,
+  `Total_Score` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `highscore`
+--
+
+INSERT INTO `highscore` (`id`, `Username`, `Score`, `Duration`, `Total_Score`) VALUES
+(1, 'doyok', 81, 88, 169),
+(2, 'bejo', 75, 60, 135),
+(3, 'a', 60, 72, 132),
+(4, 's', 60, 74, 134),
+(5, 'd', 55, 59, 114);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `highscore`
+--
+ALTER TABLE `highscore`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `highscore`
+--
+ALTER TABLE `highscore`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
